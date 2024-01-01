e9bd026e6bd216dbbb289475cc14019775941c44
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-01-01 17:53:09 +0530

    Update in fields

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,10 @@ import {HomeComponent} from "./user/home/home.component";
 import {TaskComponent} from "./user/task/task.component";
 import {TaskListComponent} from "./user/task/task-list/task-list.component";
 import {TaskFormComponent} from "./user/task/task-form/task-form.component";
+import {AdminDashboardComponent} from "./admin/admin-dashboard/admin-dashboard.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {AccountComponent} from "./admin/account/account.component";
+import {AdminHeaderComponent} from "./admin/admin-header/admin-header.component";
 
 export const routes: Routes = [
   {
@@ -36,5 +40,23 @@ export const routes: Routes = [
         ]
       }
     ]
-  }
+  },
+  {
+    path: 'admin',
+    component: AdminHeaderComponent,
+    children: [
+      {
+        path: '',
+        component: AdminDashboardComponent
+      },
+      {
+        path: 'accounts',
+        component: AccountComponent
+      }
+    ]
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
 ];
